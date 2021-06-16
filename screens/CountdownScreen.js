f6198248c8aacd9e7fe2378b83ab42fab72a9e10
CredoSpero <64059669+CredoSpero@users.noreply.github.com>
2021-06-16 14:57:16 +0800

    added pic

--- screens/CountdownScreen.js
+++ screens/CountdownScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 function CountdownScreen({ navigation }) {
@@ -12,7 +12,9 @@ function CountdownScreen({ navigation }) {
         backgroundColor: "lightblue",
       }}
     >
-      <Text> Countdown! </Text>
+       <Image style = {styles.image} source = {{uri: "https://preview.pixlr.com/images/800wm/100/1/1001503340.jpg"}} />
+      <Text> Countdown timer! </Text>
+
       <Button
         title="You've won $1MM!!!! CLICK HERE!!!"
         onPress={() => navigation.navigate("Note")}
@@ -35,3 +37,13 @@ export default function CountdownStack() {
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 20,
+  },
+  
+});
